0f4f90afa815428630127b8e42c7cd5a3b578efe
kashir <kashir.sami.7e2@itb.cat>
2021-03-02 18:58:32 +0100

    project_base_joint_version_0.0.4

--- app/src/main/java/cat/itb/instagramclone/adapters/StoryAdapter.java
+++ app/src/main/java/cat/itb/instagramclone/adapters/StoryAdapter.java
@@ -1,4 +1,54 @@
 package cat.itb.instagramclone.adapters;
 
-public class StoryAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
+
+import cat.itb.instagramclone.R;
+import cat.itb.instagramclone.models.Story;
+
+public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.ViewHolder> {
+    List<Story> storyList;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.story_home_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bindData(this.storyList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.storyList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imagen_usuario;
+        MaterialTextView nombre_usuario;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagen_usuario = itemView.findViewById(R.id.imagen_user_story_item);
+            nombre_usuario = itemView.findViewById(R.id.nombre_user_story_item);
+        }
+
+        public void bindData(Story s){
+            imagen_usuario.setImageResource(s.getUser_story().getImagen_usuario().getImageAlpha());
+            nombre_usuario.setText(s.getUser_story().getNombre_usuario());
+        }
+    }
 }
