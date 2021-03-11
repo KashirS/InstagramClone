292b05f0008d3f9796363671fc1b6b7051bd97e8
Kashir <kashir.sami.7e2@itb.cat>
2021-03-11 17:02:09 +0100

    aqui hemos hecho el tema de navegacion y las funciones de login y register.

--- app/src/main/java/cat/itb/instagramclone/fragments/LoginFragment.java
+++ app/src/main/java/cat/itb/instagramclone/fragments/LoginFragment.java
@@ -1,20 +1,32 @@
 package cat.itb.instagramclone.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 import cat.itb.instagramclone.R;
 
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener{
 
-    Fragment fragment;
-
+    private TextInputEditText username;
+    private TextInputEditText password;
+    private MaterialButton login;
+    private MaterialButton register;
+    private MaterialButton forgotPassword;
+    private TextInputLayout userInput;
+    private TextInputLayout passInput;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,54 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        username = v.findViewById(R.id.user_name_login_editext);
+        password = v.findViewById(R.id.user_password_login_editext);
+        login = v.findViewById(R.id.login_button);
+        register = v.findViewById(R.id.register_button_login);
+        userInput = v.findViewById(R.id.user_name_login);
+        passInput = v.findViewById(R.id.user_password_login);
+
+
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
+
         return v;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_button:
+                verifier();
+                break;
+            case R.id.register_button_login:
+                Navigation.findNavController(getView()).navigate(R.id.action_loginFragment_to_registerFragment);
+                break;
+        }
+    }
+
+    private void verifier() {
+        String usernameVerify;
+        String passwordVerify;
+
+        usernameVerify = username.getText().toString();
+        passwordVerify = password.getText().toString();
+
+        if (usernameVerify.isEmpty()) {
+            userInput.setError("required field");
+            userInput.isEnabled();
+        }else if (passwordVerify.isEmpty()){
+            userInput.setErrorEnabled(false);
+            passInput.setError("required field");
+            passInput.isEnabled();
+        }else if (passwordVerify.length() < 8){
+            passInput.setErrorEnabled(false);
+            passInput.setError("min 8 character");
+            passInput.isEnabled();
+        }else {
+            Navigation.findNavController(getView()).navigate(R.id.action_loginFragment_to_homeFragment);
+        }
+    }
 }
--- app/src/main/java/cat/itb/instagramclone/fragments/RegisterFragment.java
+++ app/src/main/java/cat/itb/instagramclone/fragments/RegisterFragment.java
@@ -1,41 +1,137 @@
 package cat.itb.instagramclone.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 import cat.itb.instagramclone.R;
 
 
-public class RegisterFragment extends Fragment {
+public class RegisterFragment extends Fragment implements View.OnClickListener{
 
-    Fragment fragment;
+    private TextInputEditText username;
+    private TextInputLayout usernameLayout;
+    private TextInputEditText password;
+    private TextInputLayout passwordLayout;
+    private TextInputEditText repeatPassword;
+    private TextInputLayout repeatPasswordLayout;
+    private TextInputEditText email;
+    private TextInputLayout emailLayout;
+    private TextInputEditText name;
+    private TextInputLayout nameLayout;
+    private TextInputEditText surname;
+    private TextInputLayout surnameLayout;
+    private MaterialCheckBox terms;
+    private MaterialButton register;
+    private MaterialButton login;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState == null){
-            fragment = new Fragment();
-            changeFragment(fragment);
-        }
-    }
-
-    private void changeFragment(Fragment fragment) {
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+
+        username = v.findViewById(R.id.register_username);
+        usernameLayout = v.findViewById(R.id.outlined_username);
+
+        password = v.findViewById(R.id.register_password);
+        passwordLayout = v.findViewById(R.id.outlined_password);
+
+        repeatPassword = v.findViewById(R.id.register_repassword);
+        repeatPasswordLayout = v.findViewById(R.id.outlined_repassword);
+
+        email = v.findViewById(R.id.register_email);
+        emailLayout = v.findViewById(R.id.outlined_email);
+
+        name = v.findViewById(R.id.register_name);
+        nameLayout = v.findViewById(R.id.outlined_name);
+
+        surname = v.findViewById(R.id.register_surname);
+        surnameLayout = v.findViewById(R.id.outlined_surname);
+
+        terms = v.findViewById(R.id.terms_checkBox);
+        register = v.findViewById(R.id.register_register);
+        login = v.findViewById(R.id.login_button_register);
+
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register_register:
+                verifyAll();
+                Navigation.findNavController(getView()).navigate(R.id.action_registerFragment_to_homeFragment);
+                break;
+            case R.id.login_button_register:
+                Navigation.findNavController(getView()).navigate(R.id.action_registerFragment_to_loginFragment);
+                break;
+        }
+    }
+    private void verifyAll() {
+        String userVerify, passwordVerify, repeatPasswordVer, emailVerify, nameVerify, surnameVerify;
+
+        userVerify = username.getText().toString();
+        passwordVerify = password.getText().toString();
+        repeatPasswordVer = repeatPassword.getText().toString();
+        emailVerify = email.getText().toString();
+        nameVerify = name.getText().toString();
+        surnameVerify = surname.getText().toString();
+
+        if (userVerify.isEmpty()){
+            usernameLayout.setError("required field");
+            usernameLayout.isEnabled();
+        }else if (passwordVerify.isEmpty()){
+            usernameLayout.setErrorEnabled(false);
+            passwordLayout.setError("required field");
+            passwordLayout.isEnabled();
+        }else if (repeatPasswordVer.isEmpty()){
+            passwordLayout.setErrorEnabled(false);
+            repeatPasswordLayout.setError("required field");
+            repeatPasswordLayout.isEnabled();
+        }else if (!(passwordVerify.equals(passwordVerify))){
+            repeatPasswordLayout.setErrorEnabled(false);
+            repeatPasswordLayout.setError("password isn't equal");
+            repeatPasswordLayout.isEnabled();
+        }else if (emailVerify.isEmpty()){
+            repeatPasswordLayout.setErrorEnabled(false);
+            emailLayout.setError("required field");
+            emailLayout.isEnabled();
+        }else if (nameVerify.isEmpty()){
+            emailLayout.setErrorEnabled(false);
+            nameLayout.setError("required field");
+            nameLayout.isEnabled();
+        }else if (surnameVerify.isEmpty()){
+            nameLayout.setErrorEnabled(false);
+            surnameLayout.setError("required field");
+            surnameLayout.isEnabled();
+        }else {
+            surnameLayout.setErrorEnabled(false);
+        }
+
     }
 }
