c83ce0613b5461d5963e7bcd1359c958507ff3ea
KashirS <kashir.sami.7e2@itb.cat>
2021-03-11 13:27:13 +0100

    jorge changes

--- app/src/main/java/cat/itb/instagramclone/activities/MainActivity.java
+++ app/src/main/java/cat/itb/instagramclone/activities/MainActivity.java
@@ -9,22 +9,11 @@ import cat.itb.instagramclone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null){
-            currentFragment = new Fragment();
-            changeFragment();
-        }
-
-    }
-
-    private void changeFragment() {
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,currentFragment).commit();
     }
 }
--- app/src/main/java/cat/itb/instagramclone/adapters/ActivityAdapter.java
+++ app/src/main/java/cat/itb/instagramclone/adapters/ActivityAdapter.java
@@ -1,4 +1,53 @@
 package cat.itb.instagramclone.adapters;
 
-public class ActivityAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
+
+import cat.itb.instagramclone.R;
+import cat.itb.instagramclone.models.Notification;
+
+public class ActivityAdapter extends RecyclerView.Adapter<ActivityAdapter.ViewHolder> {
+    List<Notification> notificationList;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_notificacion_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bindData(this.notificationList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.notificationList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imagen_user_notification;
+        MaterialTextView texto_notificacion;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagen_user_notification = itemView.findViewById(R.id.user_image_notification);
+            texto_notificacion = itemView.findViewById(R.id.text_notification);
+        }
+
+        public void bindData(Notification not){
+            imagen_user_notification.setImageResource(not.getUser_notification().getImagen_usuario().getImageAlpha());
+            texto_notificacion.setText(not.getNotification());
+        }
+    }
 }
