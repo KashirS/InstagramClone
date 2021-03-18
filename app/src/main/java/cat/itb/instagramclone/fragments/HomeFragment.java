13c0eee86d53d2b29aed20da7694f6e20eb1bbab
kashirS <kashir.sami.7e2@itb.cat>
2021-03-18 17:21:16 +0100

    modificaciones de home page

--- app/src/main/java/cat/itb/instagramclone/fragments/HomeFragment.java
+++ app/src/main/java/cat/itb/instagramclone/fragments/HomeFragment.java
@@ -28,7 +28,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View v = inflater.inflate(R.layout.home_fragment, container, false);
+        return v;
     }
 
     @Override
