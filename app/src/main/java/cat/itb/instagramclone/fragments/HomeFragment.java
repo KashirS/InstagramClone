a04dca4d2d752f477dba834d623dfb070d0d645f
KashirS <kashir.sami.7e2@itb.cat>
2021-03-21 18:23:24 +0100

    top_app_bar home

--- app/src/main/java/cat/itb/instagramclone/fragments/HomeFragment.java
+++ app/src/main/java/cat/itb/instagramclone/fragments/HomeFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,12 +23,14 @@ import android.view.ViewGroup;
 import cat.itb.instagramclone.R;
 import cat.itb.instagramclone.activities.MainActivity;
 import cat.itb.instagramclone.adapters.PublicationAdapter;
+import cat.itb.instagramclone.adapters.StoryAdapter;
 import cat.itb.instagramclone.viewmodel.HomeViewModel;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel mViewModel;
     RecyclerView publicaciones_recyclerView;
+    RecyclerView story;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -48,6 +51,11 @@ public class HomeFragment extends Fragment {
         publicaciones_recyclerView = v.findViewById(R.id.home_publication_recyclerView);
         publicaciones_recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         publicaciones_recyclerView.setAdapter(new PublicationAdapter(mViewModel.publicacionesList));
+
+        story = v.findViewById(R.id.story_recy);
+        story.setLayoutManager(new GridLayoutManager(getContext(),1));
+        story.setAdapter(new StoryAdapter());
+
         return v;
     }
 
